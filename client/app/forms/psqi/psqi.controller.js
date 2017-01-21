d9b91d090ced213d759d27742d6325e5e801fb93
ArthurLnq <arthur.lannelucq&outlook.fr>
2017-01-21 13:13:48 +0100

    Ajout méthode getFormattedTimepicker()

--- client/app/forms/psqi/psqi.controller.js
+++ client/app/forms/psqi/psqi.controller.js
@@ -13,13 +13,18 @@ class PsqiComponent extends Card{
       {label: "3 ou 4 fois par semaine", value: 4}
     ];
   }
+
+  getFormattedTimepicker() {
+    this.formattedSleepingHour = this.usualSleepingHour.getHours() + ':' + ('0'+this.usualSleepingHour.getMinutes()).slice(-2);
+    this.formattedWakeUpHour = this.usualWakeUpHour.getHours() + ':' + ('0'+ this.usualWakeUpHour.getMinutes()).slice(-2);
+  }
 }
 
 angular.module('angularMaterialClinidamApp')
   .component('psqi', {
     templateUrl: 'app/forms/psqi/psqi.html',
     controller: PsqiComponent,
-    controllerAs: 'psqiCtrl'
+    controllerAs: 'vm'
   });
 
 })();
