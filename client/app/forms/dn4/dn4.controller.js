04f1c20a9e22d2f297af05c88b536cb416beefe1
ArthurLnq <arthur.lannelucq&outlook.fr>
2017-01-21 13:12:07 +0100

    Ajout méthode isFilledIn utilisée lors de la création du pdf dans dn4.controller.js

--- client/app/forms/dn4/dn4.controller.js
+++ client/app/forms/dn4/dn4.controller.js
@@ -8,11 +8,18 @@ class Dn4Component extends Card{
         this.answers = [
             {label: "Oui", value: 'OUI'},
             {label: "Non", value: 'NON'}
-        ]
+        ];
+    }
+
+    isFilledIn(field) {
+        if( typeof field !== 'undefined' ) {
+            return field;
+        } else {
+            return "N/D";
+        }
     }
 
     openPdf() {
-
         this.createPdf();
         pdfMake.createPdf(this.pdf).open();
     }
@@ -29,8 +36,8 @@ class Dn4Component extends Card{
                      text: '____________________________________________________________________'
                  },
 
-                    'Nom : ' + this.patientLastname,
-                    'Prénom : ' + this.patientFirstname,
+                    'Nom : ' + this.isFilledIn(this.patientLastname),
+                    'Prénom : ' + this.isFilledIn(this.patientFirstname),
 
 
                  { text: 'La douleur présente-t-elle des :', style: 'header' },
@@ -39,11 +46,11 @@ class Dn4Component extends Card{
                      text: '____________________________________________________________________'
                  },
 
-                 'Brûlures ? ' + this.hasBurns,
+                 'Brûlures ? ' + this.isFilledIn(this.hasBurns),
 
-                 'Sensations de froid douloureux ? ' + this.hasPainCold,
+                 'Sensations de froid douloureux ? ' + this.isFilledIn(this.hasPainCold),
 
-                 'Décharges électriques ? ' + this.hasElectricShock,
+                 'Décharges électriques ? ' + this.isFilledIn(this.hasElectricShock),
 
                  { text: 'La douleur est-elle associée dans la même région à des :', style: 'header' },
                  {
@@ -51,13 +58,13 @@ class Dn4Component extends Card{
                      text: '____________________________________________________________________'
                  },
 
-                 'Fourmillements ? ' + this.swarming,
+                 'Fourmillements ? ' + this.isFilledIn(this.swarming),
 
-                 'Picotements ? ' + this.tingling,
+                 'Picotements ? ' + this.isFilledIn(this.tingling),
 
-                 'Engourdissements ? ' + this.numbness,
+                 'Engourdissements ? ' + this.isFilledIn(this.numbness),
 
-                 'Démangeaisons ? ' + this.itching,
+                 'Démangeaisons ? ' + this.isFilledIn(this.itching),
 
                  { text: 'La douleur est-elle localisée dans un territoire où l\'examen met en évidence une :', style: 'header' },
                  {
@@ -65,16 +72,16 @@ class Dn4Component extends Card{
                      text: '____________________________________________________________________'
                  },
 
-                 'Hypoesthésie au tact ? ' + this.tactHypoesthesia,
+                 'Hypoesthésie au tact ? ' + this.isFilledIn(this.tactHypoesthesia),
 
-                 'Hypoesthésie à la piqûre ? ' + this.stingHypoesthesia,
+                 'Hypoesthésie à la piqûre ? ' + this.isFilledIn(this.stingHypoesthesia),
 
                  {
                      style: 'border',
                      text: '____________________________________________________________________'
                  },
 
-                 {text: 'La douleur est-elle provoquée ou augmentée par un frottement ? ' + this.friction, style: 'header' }
+                 'La douleur est-elle provoquée ou augmentée par un frottement ? ' + this.isFilledIn(this.friction)
 
              ],
 
